ff1f3b780e933c9e83e78680430041286d68b1d6
Oskar Roman <oskar@formida.se>
2020-08-13 12:35:37 +0200

    move aws ops to own file

---
+++ cmd/sec-group-updater/awsOps.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+func fetchSecurityGroups(svc *ec2.EC2) ([]*ec2.SecurityGroup, error) {
+	sc, err := svc.DescribeSecurityGroups(&ec2.DescribeSecurityGroupsInput{})
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("Unable to describe VPCs, %v", err))
+	}
+	if len(sc.SecurityGroups) == 0 {
+		return nil, errors.New("No VPCs found needle")
+	}
+	return sc.SecurityGroups, nil
+}
+func createSession() *ec2.EC2 {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	return ec2.New(sess)
+}
+
+func deleteIPRange(groupName *string, tag string, port int64, svc *ec2.EC2, ipPermissions []*ec2.IpPermission) error {
+	for _, v := range ipPermissions {
+		if aws.Int64Value(v.FromPort) == port {
+			for _, v2 := range v.IpRanges {
+				if aws.StringValue(v2.Description) == tag {
+					fmt.Printf("Tag %s found in %s with ip-range %s\n", aws.StringValue(v2.Description), aws.StringValue(groupName), aws.StringValue(v2.CidrIp))
+					err := deAuth(groupName, v2.CidrIp, port, svc)
+					if err != nil {
+						return err
+					}
+					fmt.Printf("IP-range %s deleted in group %s\n", aws.StringValue(v2.CidrIp), aws.StringValue(groupName))
+				}
+			}
+		}
+	}
+	return nil
+}
+func deAuth(groupName, cidrIP *string, port int64, svc *ec2.EC2) error {
+	input := &ec2.RevokeSecurityGroupIngressInput{
+		CidrIp:     cidrIP,
+		ToPort:     aws.Int64(port),
+		FromPort:   aws.Int64(port),
+		GroupName:  groupName,
+		IpProtocol: aws.String("tcp"),
+	}
+	_, err := svc.RevokeSecurityGroupIngress(input)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Unable to revoke security group %v ingress, %v", cidrIP, err))
+	}
+	return nil
+}
+
+func auth(ip string, port int64, groupName *string, svc *ec2.EC2) error {
+	_, err := svc.AuthorizeSecurityGroupIngress(&ec2.AuthorizeSecurityGroupIngressInput{
+		GroupName: groupName,
+		IpPermissions: []*ec2.IpPermission{
+			// Can use setters to simplify seting multiple values without the
+			// needing to use aws.String or associated helper utilities.
+			(&ec2.IpPermission{}).
+				SetIpProtocol("tcp").
+				SetFromPort(port).
+				SetToPort(port).
+				SetIpRanges([]*ec2.IpRange{
+					{
+						Description: aws.String("oskar mobile"),
+						CidrIp:      aws.String(ip + "/32"),
+					},
+				}),
+		},
+	})
+	if err != nil {
+		return errors.New(fmt.Sprintf("Unable to set security group %v ingress, %v", groupName, err))
+	}
+	fmt.Printf("IP-range %s/32 port %d added in group %s\n", ip, port, aws.StringValue(groupName))
+	return nil
+}
--- cmd/sec-group-updater/main.go
+++ cmd/sec-group-updater/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -11,8 +10,6 @@ import (
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
 func main() {
@@ -26,10 +23,6 @@ func main() {
 	flag.BoolVar(&deleteFlag, "delete", true, "delete ingress rule with description tag before setting new")
 	flag.Parse()
 	securityGroups := strings.Split(sg, ",")
-	if len(securityGroups) == 0 {
-		fmt.Printf("No security groups provided --sec-groups flag\n")
-		os.Exit(-1)
-	}
 	if tag == "" {
 		fmt.Print("No tag provided user the --tag flag\n")
 		os.Exit(-1)
@@ -52,7 +45,7 @@ func main() {
 
 	//looking for security groups that match the provided names. if a match is found current ingress rules is deleted and a new one is created with your new ip
 	for _, v := range sgs {
-		if inArray(aws.StringValue(v.GroupName), securityGroups) {
+		if inArray(aws.StringValue(v.GroupName), securityGroups) || len(securityGroups) == 0 {
 			if deleteFlag {
 				if err := deleteIPRange(v.GroupName, tag, int64(port), svc, v.IpPermissions); err != nil {
 					log.Fatalf("Error while deleting ip-range %+v", err)
@@ -66,23 +59,6 @@ func main() {
 	fmt.Printf("Ingess rules updated\n")
 }
 
-func fetchSecurityGroups(svc *ec2.EC2) ([]*ec2.SecurityGroup, error) {
-	sc, err := svc.DescribeSecurityGroups(&ec2.DescribeSecurityGroupsInput{})
-	if err != nil {
-		return nil, errors.New(fmt.Sprintf("Unable to describe VPCs, %v", err))
-	}
-	if len(sc.SecurityGroups) == 0 {
-		return nil, errors.New("No VPCs found needle")
-	}
-	return sc.SecurityGroups, nil
-}
-func createSession() *ec2.EC2 {
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-
-	return ec2.New(sess)
-}
 func inArray(needle string, haystack []string) bool {
 	for _, v := range haystack {
 		if v == needle {
@@ -103,60 +79,4 @@ func findMyIP() (string, error) {
 		return "", err
 	}
 	return string(ip), nil
-}
-func deleteIPRange(groupName *string, tag string, port int64, svc *ec2.EC2, ipPermissions []*ec2.IpPermission) error {
-	for _, v := range ipPermissions {
-		if aws.Int64Value(v.FromPort) == port {
-			for _, v2 := range v.IpRanges {
-				if aws.StringValue(v2.Description) == tag {
-					fmt.Printf("Tag %s found in %s with ip-range %s\n", aws.StringValue(v2.Description), aws.StringValue(groupName), aws.StringValue(v2.CidrIp))
-					err := deAuth(groupName, v2.CidrIp, port, svc)
-					if err != nil {
-						return err
-					}
-					fmt.Printf("IP-range %s deleted in group %s\n", aws.StringValue(v2.CidrIp), aws.StringValue(groupName))
-				}
-			}
-		}
-	}
-	return nil
-}
-func deAuth(groupName, cidrIP *string, port int64, svc *ec2.EC2) error {
-	input := &ec2.RevokeSecurityGroupIngressInput{
-		CidrIp:     cidrIP,
-		ToPort:     aws.Int64(port),
-		FromPort:   aws.Int64(port),
-		GroupName:  groupName,
-		IpProtocol: aws.String("tcp"),
-	}
-	_, err := svc.RevokeSecurityGroupIngress(input)
-	if err != nil {
-		return errors.New(fmt.Sprintf("Unable to revoke security group %v ingress, %v", cidrIP, err))
-	}
-	return nil
-}
-
-func auth(ip string, port int64, groupName *string, svc *ec2.EC2) error {
-	_, err := svc.AuthorizeSecurityGroupIngress(&ec2.AuthorizeSecurityGroupIngressInput{
-		GroupName: groupName,
-		IpPermissions: []*ec2.IpPermission{
-			// Can use setters to simplify seting multiple values without the
-			// needing to use aws.String or associated helper utilities.
-			(&ec2.IpPermission{}).
-				SetIpProtocol("tcp").
-				SetFromPort(port).
-				SetToPort(port).
-				SetIpRanges([]*ec2.IpRange{
-					{
-						Description: aws.String("oskar mobile"),
-						CidrIp:      aws.String(ip + "/32"),
-					},
-				}),
-		},
-	})
-	if err != nil {
-		return errors.New(fmt.Sprintf("Unable to set security group %v ingress, %v", groupName, err))
-	}
-	fmt.Printf("IP-range %s/32 port %d added in group %s\n", ip, port, aws.StringValue(groupName))
-	return nil
 }
